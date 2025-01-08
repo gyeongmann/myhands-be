aaaf166aaba4012181f681d4b18314bc853a90fd
김현지 <chik1y@naver.com>
2025-01-08 21:58:03 +0900

    [fix] application.properties 충돌 해결(#3)

--- myhands/src/main/java/tabom/myhands/MyhandsApplication.java
+++ myhands/src/main/java/tabom/myhands/MyhandsApplication.java
@@ -3,9 +3,11 @@ package tabom.myhands;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class MyhandsApplication {
 
 	public static void main(String[] args) {
