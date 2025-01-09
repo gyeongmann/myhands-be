2c8d449203f634b5ff4261acd0d59449980677cb
gyeongmann <hkc8857@naver.com>
2025-01-09 16:59:57 +0900

    [feat] Quest 완료 DTO 생성(#7)

--- myhands/src/main/java/tabom/myhands/domain/quest/dto/QuestRequest.java
+++ myhands/src/main/java/tabom/myhands/domain/quest/dto/QuestRequest.java
@@ -4,7 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 public class QuestRequest {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CompleteQuest {
+        private Long QuestId;
+        private String grade;
+        private Integer expAmount;
+        private Boolean isCompleted;
+        private LocalDateTime completedAt;
+    }
 
     @Getter
     @NoArgsConstructor
