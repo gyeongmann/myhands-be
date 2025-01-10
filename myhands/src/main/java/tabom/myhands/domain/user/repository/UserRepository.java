faf38ed058bf9ae64618558dd17cfdda41f88b25
gyeongmann <hkc8857@naver.com>
2025-01-10 14:56:23 +0900

    [chore] import 수정(#7)
    Signed-off-by: gyeongmann <hkc8857@naver.com>

--- myhands/src/main/java/tabom/myhands/domain/user/repository/UserRepository.java
+++ myhands/src/main/java/tabom/myhands/domain/user/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import tabom.myhands.domain.user.entity.User;
 
-import java.util.List;
 import java.util.Optional;
 
 
