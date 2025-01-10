f55e9a9e8ee80a3ce70b8430cb6ffb2ff3446171
김현지 <chik1y@naver.com>
2025-01-10 17:00:55 +0900

    [feat] 회원 로그아웃 구현(#20)

--- myhands/src/main/java/tabom/myhands/domain/user/controller/UserController.java
+++ myhands/src/main/java/tabom/myhands/domain/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package tabom.myhands.domain.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tabom.myhands.common.jwt.TokenUtils;
 import tabom.myhands.common.response.DtoResponse;
 import tabom.myhands.common.response.MessageResponse;
 import tabom.myhands.domain.user.dto.UserRequest;
@@ -35,5 +37,14 @@ public class UserController {
     public ResponseEntity<DtoResponse<UserResponse.Login>> loginUser(@RequestBody UserRequest.Login request) {
         UserResponse.Login response = userService.login(request);
         return ResponseEntity.status(HttpStatus.OK).body(DtoResponse.of(HttpStatus.OK, responseProperties.getSuccess(), response));
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<MessageResponse> logoutUser(HttpServletRequest request, @RequestHeader("Authorization") String accessTokenHeader) {
+        Long userId = (Long) request.getAttribute("userId");
+        boolean isAdmin = (boolean) request.getAttribute("isAdmin");
+        String accessToken = TokenUtils.extractToken(accessTokenHeader);
+        userService.logout(userId, isAdmin, accessToken);
+        return ResponseEntity.status(HttpStatus.OK).body(MessageResponse.of(HttpStatus.OK, responseProperties.getSuccess()));
     }
 }
